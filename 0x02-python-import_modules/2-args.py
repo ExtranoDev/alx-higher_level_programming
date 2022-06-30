b7ae5e88128c5ceb74b61b6d12a801f8e988c752
ExtranoDev <jeomade19@gmail.com>
2022-06-30 23:17:47 +0000

    Arguments exercise

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    len_arg = len(sys.argv) - 1
-    str_arg = "arguments"
-    if len_arg == 0:
-        print("0 {}.".format(str_arg))
-    elif len_arg > 0:
-        print("{:d} {}:".format(len_arg, "argument" if len_arg == 1 else str_arg))
+    len_a = len(sys.argv) - 1
+    str_a = "arguments"
+    if len_a == 0:
+        print("0 {}.".format(str_a))
+    elif len_a > 0:
+        print("{:d} {}:".format(len_a, "argument" if len_a == 1 else str_a))
         count = 0
         for i in sys.argv:
             if i == sys.argv[0]:
