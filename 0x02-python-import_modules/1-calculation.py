a066a5b80c7144727f63627d12c551501ff64696
ExtranoDev <jeomade19@gmail.com>
2022-06-30 22:44:44 +0000

    Import calculator

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,9 +1,9 @@
-#!/usr/bin/python3
+i#!/usr/bin/python3
 if __name__ = "__main__":
     import calculator_1 as cals
     a = 10
     b = 5
-    print("{} {} {}".format(a, "+", b, cals.add(a, b)))
-    print("{} {} {}".format(a, "-", b, cals.sub(a, b)))
-    print("{} {} {}".format(a, "*", b, cals.mul(a, b)))
-    print("{} {} {}".format(a, "/", b, cals.div(a, b)))
+    print("{:d} {} {:d} {:d}".format(a, "+", b, cals.add(a, b)))
+    print("{:d} {} {:d} {:d}".format(a, "-", b, cals.sub(a, b)))
+    print("{:d} {} {:d} {:d}".format(a, "*", b, cals.mul(a, b)))
+    print("{:d} {} {:d} {:d}".format(a, "/", b, cals.div(a, b)))
