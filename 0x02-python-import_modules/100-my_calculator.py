f6c72542f385d22874f86d506c0f1e98415cb279
ExtranoDev <jeomade19@gmail.com>
2022-07-01 14:16:01 +0000

    Correct pycodestyle

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -2,27 +2,27 @@
 if __name__ == "__main__":
     import sys
     from calculator_1 import add, sub, mul, div
-    
-len_arg = len(sys.argv)
 
-if len_arg != 4:
-    print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-    sys.exit(1)
+    len_arg = len(sys.argv)
 
-ops = sys.argv[2]
-if ops not in "+-*/":
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
+    if len_arg != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
 
-a = int(sys.argv[1])
-b = int(sys.argv[3])
-out_print = "{:d} {} {:d} = {:d}"
+    ops = sys.argv[2]
+    if ops not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
 
-if ops == "+":
-    print(out_print.format(a, "+", b, add(a, b)))
-elif ops == "-":
-    print(out_print.format(a, "-", b, sub(a, b)))
-elif ops == "*":
-    print(out_print.format(a, "*", b, mul(a, b)))
-elif ops == "/":
-    print(out_print.format(a, "/", b, div(a, b)))
+    a = int(sys.argv[1])
+    b = int(sys.argv[3])
+    out_print = "{:d} {} {:d} = {:d}"
+
+    if ops == "+":
+        print(out_print.format(a, "+", b, add(a, b)))
+    elif ops == "-":
+        print(out_print.format(a, "-", b, sub(a, b)))
+    elif ops == "*":
+        print(out_print.format(a, "*", b, mul(a, b)))
+    elif ops == "/":
+        print(out_print.format(a, "/", b, div(a, b)))
