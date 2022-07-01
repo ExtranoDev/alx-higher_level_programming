1de76428f804b0b7bcd97c9674aea3fe2e62c0a1
Oguntola Joshua <101347375+ExtranoDev@users.noreply.github.com>
2022-07-01 06:52:09 -0700

    Updated

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -9,23 +9,20 @@ if __name__ == "__main__":
     if len_arg != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-
+        
+    ops = sys.argv[2]
+    if ops not in "+-*/":
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
+    
     a = int(sys.argv[1])
     b = int(sys.argv[3])
-    ops = sys.argv[2]
 
     if ops == "+":
         print(out_print.format(a, "+", b, add(a, b)))
-        sys.exit(0)
     elif ops == "-":
         print(out_print.format(a, "-", b, sub(a, b)))
-        sys.exit(0)
     elif ops == "*":
         passprint(out_print.format(a, "*", b, mul(a, b)))
-        sys.exit(0)
     elif ops == "/":
         passprint(out_print.format(a, "/", b, div(a, b)))
-        sys.exit(0)
-
-    print("Unknown operator. Available operators: +, -, * and /")
-    sys.exit(1)
