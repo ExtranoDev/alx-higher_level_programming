b04d3b7d1d7d6bbda236375d717c298943f91cd0
ExtranoDev <jeomade19@gmail.com>
2022-07-03 15:33:19 +0000

    Adds tuple element

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,15 +1,7 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a or tuple_b:
-        temp = [tuple_a, tuple_b]
-        idx = 0
-        for i in temp:
-            if len(i) < 2:
-                if len(i) == 0:
-                    temp[idx] = (0, 0)
-                else:
-                    temp[idx] = (temp[idx][0], 0)
-            idx = idx + 1
-        return (temp[0][0] + temp[1][0], temp[0][1] + temp[1][1])
-    else:
-        return ()
+    while len(tuple_a) < 2:
+        tuple_a += (0,)
+    while len(tuple_b) < 2:
+        tuple_b += (0,)
+    return (tuple_a[0] + tuple_b[0], tuple_a[1] + tuple_b[1])
