6c66c7ee4f5acc9f240763fe687ef3adeadb6132
ExtranoDev <jeomade19@gmail.com>
2022-07-07 01:13:05 +0000

    Sum and divide some elements

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def weight_average(my_list=[]):
-    return sum([i*j for i, j in my_list])/\
-                sum([j[1] for j in my_list])
+    return (sum([i*j for i, j in my_list])) /\
+                (sum([j[1] for j in my_list]))
