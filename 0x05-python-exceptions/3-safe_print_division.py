3edea7a470a1ed1e9b16d0e8c3e3e151bf38e964
ExtranoDev <jeomade19@gmail.com>
2022-07-23 23:28:37 +0000

    Divide safely

--- 0x05-python-exceptions/3-safe_print_division.py
+++ 0x05-python-exceptions/3-safe_print_division.py
@@ -8,13 +8,3 @@ def safe_print_division(a, b):
         return (rt_res)
     finally:
         print("Inside result: {}".format(rt_res))
-
-a = 12
-b = 2
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
-
-a = 12
-b = 0
-result = safe_print_division(a, b)
-print("{:d} / {:d} = {}".format(a, b, result))
