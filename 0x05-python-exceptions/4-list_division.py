7a7793b6fbb237e55ebcdc1cde8da1ea50745d76
ExtranoDev <jeomade19@gmail.com>
2022-07-23 23:57:44 +0000

    Divides list while catching errors

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -17,4 +17,5 @@ def list_division(my_list_1, my_list_2, list_length):
             print("out of range")
             continue
         finally:
-            return (n_list)
+            continue
+    return (n_list)
