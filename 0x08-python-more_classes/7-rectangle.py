302ef6e704d1348838b392bca2ff0e847989a30c
ExtranoDev <jeomade19@gmail.com>
2022-07-26 01:45:09 +0000

    Make pattern dynamic

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -115,27 +115,3 @@ class Rectangle:
         if self.__width > 0 and self.__height > 0:
             return 2 * (self.__width + self.__height)
         return 0
-
-my_rectangle_1 = Rectangle(8, 4)
-print(my_rectangle_1)
-print("--")
-my_rectangle_1.print_symbol = "&"
-print(my_rectangle_1)
-print("--")
-
-my_rectangle_2 = Rectangle(2, 1)
-print(my_rectangle_2)
-print("--")
-Rectangle.print_symbol = "C"
-print(my_rectangle_2)
-print("--")
-
-my_rectangle_3 = Rectangle(7, 3)
-print(my_rectangle_3)
-
-print("--")
-
-my_rectangle_3.print_symbol = ["C", "is", "fun!"]
-print(my_rectangle_3)
-
-print("--")
