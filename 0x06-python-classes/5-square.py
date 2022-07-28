ac6047589fb9cb36984d869d6d3b65dc9e26c31f
ExtranoDev <jeomade19@gmail.com>
2022-07-28 21:29:35 +0000

    Prints pattern

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""
+Class Square
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+
+    def __init__(self, size=0):
+        """
+        Class Initialization
+
+        Arguments:
+            size (int): square size
+        """
+        self.size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """
+        Calculate and returns the area of a drone
+        """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """
+        Prints pattern when called
+        """
+        for i in range(self.__size):
+            for j in range(self.__size):
+                print("#", end="")
+            print("")
+        print("")
