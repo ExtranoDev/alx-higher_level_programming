09c5dbc9a8ee5366fe73e4df12c007123ac0946c
ExtranoDev <jeomade19@gmail.com>
2022-07-28 22:11:55 +0000

    Prints with positional settings

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -57,7 +57,8 @@ class Square:
         """
         if self.__size == 0:
             print("")
-        for i in range(self.__position[1]):
-            print("")
-        for i in range(self.__size):
-            print(" " * self.position[0] + "#" * self.__size)
+        else:
+            for i in range(self.__position[1]):
+                print("")
+            for i in range(self.__size):
+                print(" " * self.position[0] + "#" * self.__size)
