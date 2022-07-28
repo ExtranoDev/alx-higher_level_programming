5f377ab7beea16f0bb5ea447ed4b213cf5f084f5
ExtranoDev <jeomade19@gmail.com>
2022-07-28 20:47:16 +0000

    Initialize private variable

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Class Square
+"""
+
+
+class Square:
+    """
+    Square class
+    """
+
+    def __init__(self, size=0):
+        """
+        Class Initialization
+
+        Arguments:
+            size (int): square size
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
