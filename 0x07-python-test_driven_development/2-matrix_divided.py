293219900e908363765b15a09c1325de7296be5a
ExtranoDev <jeomade19@gmail.com>
2022-07-30 17:28:28 +0000

    Matrix divider

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,24 +1,43 @@
 #!/usr/bin/python3
 """
+Matrix Divider
+
+Divides a Matrix by a given divisor
 """
 
 
 def matrix_divided(matrix, div):
     """
+    Divides Matrix
+
+    Args:
+        matrix (list): matrix of the format [[1, 2, 3], [4, 5, 6]]
+        div (int): Divideer
+
+    Raises:
+        TypeError: matrix must be type list of list and
+                    and elements of matrix must be integer of float
+        TypeError: Each row in matrice must be of same size
+        TypeError: divider must be a number
+        ZeroDivisionError: division by zero error
+
+    Returns:
+        list: new matrix already divided
     """
+
     if type(matrix) == list and matrix != []:
         for vals in matrix:
             if type(vals) == list:
                 if all(type(i) in (int, float) for i in vals):
                     continue
             raise TypeError("matrix must be a matrix (list of lists)"
-                            "of integers/floats")
+                            " of integers/floats")
     else:
         raise TypeError("matrix must be a matrix (list of lists)"
-                        "of integers/floats")
+                        " of integers/floats")
     if not all(len(matrix[0]) == len(i) for i in matrix):
         raise TypeError("Each row of the matrix must have the same size")
-    if not any([isinstance(div, i) for i in (int, float)]):
+    if type(div) not in (int, float):
         raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
