2f1cf337db5327738166a65a7ef3bfd216eacb92
ExtranoDev <jeomade19@gmail.com>
2022-07-31 01:16:24 +0000

    100-matrix_mul.py

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,10 +1,27 @@
 #!/usr/bin/python3
 """
+This Module defines a Matrix Multiplier
+
+Function returns a multiplied Matrix
 """
 
 
 def matrix_mul(m_a, m_b):
     """
+    Multiplies two matrix
+
+    Args:
+        m_a (list): first matrix
+        m_b (list): second matrix
+
+    Raises:
+        ValueError: all matrix must not be [] or [[]]
+                    all matrix must be multiplyable
+        TypeError: all matrix must be a list of lists
+                    all matrix should only contain int or floats
+                    all matrix must be of same size
+    Returns:
+        list: multiplied matrix
     """
     arg_lst = [(m_a, 'm_a'), (m_b, 'm_b')]
 
@@ -25,12 +42,11 @@ def matrix_mul(m_a, m_b):
                             "of the same size".format(nm))
 
     if len(m_a[0]) == len(m_b):
-        tp = (len(m_a), len(m_a[0]))
         vals = []
 
         for ma in m_a:
             ma_ct = 0
-            tmp_ls = [0 for j in range(tp[1])]
+            tmp_ls = [0 for j in range(len(ma))]
             for mb in m_b:
                 tmp_ct = 0
                 while tmp_ct < len(mb):
