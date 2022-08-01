17cff523fe72134d1b89b1ab7cca586656f877fc
ExtranoDev <jeomade19@gmail.com>
2022-08-01 23:34:23 +0000

    Add a function to the empty class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Module defines a BaseGeometry class
+"""
+
+
+class BaseGeometry:
+    """
+    A BaseGeometry class
+    """
+    def area(self):
+        """
+        Raises an exception
+        """
+        raise Exception("area() is not implemented")
