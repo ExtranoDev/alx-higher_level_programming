76a514973fb75687ef926382fa7ba1cff7c516d3
ExtranoDev <jeomade19@gmail.com>
2022-08-01 21:21:05 +0000

    Lookup function

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Module looksup attributes of an object
+"""
+
+def lookup(obj):
+    """
+    Looks up atributes of an object
+
+    Args:
+        obj (object): object
+
+    Returns:
+        list of attributes specific to the object
+    """
+    return dir(obj)
