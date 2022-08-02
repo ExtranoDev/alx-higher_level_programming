ca4e037fc78f31a97c69a6795be75590a851986c
ExtranoDev <jeomade19@gmail.com>
2022-08-02 00:47:49 +0000

    Reformats how square output is printed

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Class calculates square
+"""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Square class
+    """
+    def __init__(self, size):
+        """
+        Initializes the Square function and the Rectangle function
+        """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(self.__size, self.__size)
+
+    def __str__(self):
+        """
+        Reformats the default print function
+        """
+        return "[Square] {:d}/{:d}".format(self.__size, self.__size)
