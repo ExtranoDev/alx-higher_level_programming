172253a1b0c7a09ddbf980cb1d709f4bc3e4c90d
ExtranoDev <jeomade19@gmail.com>
2022-08-02 00:56:24 +0000

    Integer class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -12,5 +12,5 @@ class MyInt(int):
     def __eq__(self, value):
         return super().__ne__(value)
 
-    def __ne__(self. value):
+    def __ne__(self, value):
         return super().__eq__(value)
