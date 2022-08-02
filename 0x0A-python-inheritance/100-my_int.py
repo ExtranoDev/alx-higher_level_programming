ab1e1030d896cb13699c4ccf272c40ec09172c29
ExtranoDev <jeomade19@gmail.com>
2022-08-02 00:53:46 +0000

    Integer class

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Defines Int class
+"""
+
+
+class MyInt(int):
+    """
+    Int class
+    """
+
+    def __eq__(self, value):
+        return super().__ne__(value)
+
+    def __ne__(self. value):
+        return super().__eq__(value)
