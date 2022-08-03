f786ca81ed802b1c0f64e18e97ff3f2f19c406c0
ExtranoDev <jeomade19@gmail.com>
2022-08-03 05:15:26 +0000

    Loads attributes from file

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+A Student class file
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization method
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): integer value
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns details of a class instace in a dictionary form
+        """
+        if attrs is not None and type(attrs) is list:
+            temp_dict = dict()
+            for i in attrs:
+                if i in self.__dict__.keys():
+                    temp_dict[i] = self.__dict__.get(i)
+            return temp_dict
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        Load files like json
+        """
+        self.first_name = json[first_name]
+        self.last_name = json[last_name]
+        self.age = json[age]
