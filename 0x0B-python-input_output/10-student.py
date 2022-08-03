82324ceb9021e792d7e803fb994f9c8c4ad3a001
ExtranoDev <jeomade19@gmail.com>
2022-08-03 04:50:48 +0000

    Attribute finder

---
+++ 0x0B-python-input_output/10-student.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+"""
+A Student class file
+"""
+
+
+class Student:
+    """
+    Student class
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initialization method
+
+        Args:
+            first_name (str): first name
+            last_name (str): last name
+            age (int): integer value
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """
+        Returns details of a class instace in a dictionary form
+        """
+        if attrs != None and type(attrs) is list:
+            temp_dict = dict()
+            for i in attrs:
+                if i in self.__dict__.keys():
+                    temp_dict[i] = self.__dict__.get(i)
+            return temp_dict
+        return self.__dict__
