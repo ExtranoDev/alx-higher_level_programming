d10f78c9626d1b52a0a0e72bef155d0fa5c3c4e1
ExtranoDev <jeomade19@gmail.com>
2022-08-09 00:15:53 +0000

    Submit

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -26,6 +26,13 @@ class Base:
 
     @staticmethod
     def validator(value, nvalue):
+        """
+        Validates values in inherited classes
+
+        Args:
+            value (int): value 1
+            nvalue (str): value identity
+        """
         if type(value) is not int:
             raise TypeError("{} must be an integer".format(nvalue))
         if value <= 0 and nvalue in ("height", "width"):
@@ -35,26 +42,48 @@ class Base:
 
     @staticmethod
     def to_json_string(list_dictionaries):
+        """
+        Creates a json format of a dictionary
+
+        Args:
+            list_dictionaries (dict): dictionary details
+
+        Returns:
+            json format of dictionary details
+        """
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
-        if list_objs is not None and type(list_objs) is list:
-            if not any(isinstance(i, cls) for i in list_objs):
-                raise TypeError("")
+        """
+        Saves objects to file in json format
         
+        Args:
+            cls (class): class
+            list_objs (list): list of instances
+        """
         temp_dict = []
-        if list_objs is None:
+        if list_objs is not None:
             temp_dict = [i.to_dictionary() for i in list_objs]
-        nm = str(type(list_objs[0]).__name__) + ".json"
+        nm = cls.__name__ + ".json"
         
         with open(nm, 'w') as f:
             f.write(cls.to_json_string(temp_dict))
 
     @staticmethod
     def from_json_string(json_string):
+        """
+        Converts json str to python object
+
+        Args:
+            json_string (json): json object
+
+        Returns:
+            None if json string is None or python
+            object format of json string
+        """
         if json_string is None:
             return []
         return json.loads(json_string)
