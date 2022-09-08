1f3b20ea3d837d8d95a93b9409c274c4b34656fb
ExtranoDev <jeomade19@gmail.com>
2022-09-08 21:33:04 +0000

    Changes encoding for a database, table and column

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- change the encoding of database, table and field
 ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY name varchar(256) COLLATE utf8mb4_unicode_ci;
+ALTER TABLE first_table MODIFY name varchar(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
