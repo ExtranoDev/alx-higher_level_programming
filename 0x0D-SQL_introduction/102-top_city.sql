294ab34ba4fd3f3be1f2bc72582ed2107acb8e19
ExtranoDev <jeomade19@gmail.com>
2022-09-09 01:13:45 +0000

    top citites with high temperatur

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,3 +1,2 @@
 -- script displays top 3  cities temperatur in july and august
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR mont
-h = 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 OR month = 8 GROUP BY city ORDER BY AVG(value) DESC LIMIT 3;
