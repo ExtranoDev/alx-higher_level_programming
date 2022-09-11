e7db6130f83fc9bcaa148b0cad15e39ac491208f
ExtranoDev <jeomade19@gmail.com>
2022-09-11 20:39:21 +0000

    Update character

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,5 +6,5 @@ COLLATE utf8mb4_unicode_ci;
 -- convert table 'first_table'
 USE hbtn_0c_0;
 ALTER TABLE first_table
-CHARACTER SET utf8mb4
+CONVERT TO CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
