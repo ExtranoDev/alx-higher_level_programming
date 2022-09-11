287c95051717979ba30af47d650094850eb7416a
ExtranoDev <jeomade19@gmail.com>
2022-09-11 20:38:02 +0000

    Update character

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -6,10 +6,5 @@ COLLATE utf8mb4_unicode_ci;
 -- convert table 'first_table'
 USE hbtn_0c_0;
 ALTER TABLE first_table
-COLLATE utf8mb4_unicode_ci;
-
--- convert field 'name' in 'first_table'
-ALTER TABLE first_table
-CHANGE name name VARCHAR(256)
 CHARACTER SET utf8mb4
 COLLATE utf8mb4_unicode_ci;
