efb92c81950686743e12e52e22a8f4a5a0a03047
ExtranoDev <jeomade19@gmail.com>
2022-10-15 23:36:20 +0000

    Adds two element

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+fArg = process.argv[2];
+sArg = process.argv[3];
+
+if (fArg === undefined || sArg === undefined) {
+  console.log('NaN');
+} else {
+  fArg = Number(fArg);
+  sArg = Number(sArg);
+  if (!(isNaN(sArg) || isNaN(fArg))) {
+    console.log(add(fArg, sArg));
+  } else {
+    console.log('NaN');
+  }
+}
+
+function add(a, b) {
+  return a + b;
+}
