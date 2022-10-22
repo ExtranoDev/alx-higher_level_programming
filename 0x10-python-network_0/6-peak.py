b96819f22cb9723add40642a9f01e3e06b32a16d
ExtranoDev <jeomade19@gmail.com>
2022-10-22 16:39:10 +0000

    Add documentation

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
-# function finds a peak in a list of unsorted integers
+""" function finds a peak in a list of unsorted integers
+"""
 
 def find_peak(list_of_integers):
+    """Function finds the peak in an unsorted array
+    """
     if list_of_integers == []:
         return None
     arr = list_of_integers
