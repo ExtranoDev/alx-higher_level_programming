91dfadc7684e89a857ced13d87ec77046ad869f7
ExtranoDev <jeomade19@gmail.com>
2022-10-28 22:26:38 +0000

    prints pattern

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let h = this.height;
+    while (h > 0) {
+      console.log('X'.repeat(this.width));
+      h -= 1;
+    }
+  }
+};
