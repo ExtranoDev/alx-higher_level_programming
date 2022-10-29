db7e280923c50d08d0b049cfe3dbc613918dc096
ExtranoDev <jeomade19@gmail.com>
2022-10-29 17:15:09 +0000

    viewed header var using requests

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""script takes in a URL, sends a request to the URL and
+displays the value of an header variable."""
+
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    r = requests.get(argv[1])
+    print(r.headers['X-Request-Id'])
