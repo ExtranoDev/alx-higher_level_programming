9afcae1041afb4da5de14c0e05de59681e870575
ExtranoDev <jeomade19@gmail.com>
2022-10-29 15:54:53 +0000

    get header variable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 
+""" script that takes in a URL,
+    sends a request to the URL and
+    displays the value of the X-Request-Id header variable
+"""
 from urllib.request import urlopen
 import sys
 
-
 url = sys.argv[1]
 with urlopen(url) as response:
-    print(response.info().get('X-Request-Id'))
+    print(response.headers.get('X-Request-Id'))
