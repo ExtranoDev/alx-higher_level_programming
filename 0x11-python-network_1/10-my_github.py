c65b57362fe1fa4dbb407367e2cb5468d60b5ed7
ExtranoDev <jeomade19@gmail.com>
2022-10-29 18:35:50 +0000

    authenticates github credentials

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+"""script that takes GitHub credentials (username and password)
+and uses the GitHub API to display your id"""
+
+import requests
+from requests.auth import HTTPBasicAuth
+from sys import argv
+
+if __name__ == '__main__':
+    auth = HTTPBasicAuth(argv[1], argv[2])
+    r = requests.get('https://api.github.com/user', auth=auth)
+    print(r.json().get('id'))
