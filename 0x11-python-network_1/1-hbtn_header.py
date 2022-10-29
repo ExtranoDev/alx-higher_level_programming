12f52ff474a9ab27d65797bcde9fc8dcafc943e2
ExtranoDev <jeomade19@gmail.com>
2022-10-29 15:56:34 +0000

    get header variable

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 
-""" script that takes in a URL,
-    sends a request to the URL and
-    displays the value of the X-Request-Id header variable
-"""
+"""script that takes in a URL,sends a request to the URL and
+displays the value of the X-Request-Id header variable."""
+
 from urllib.request import urlopen
 import sys
 
