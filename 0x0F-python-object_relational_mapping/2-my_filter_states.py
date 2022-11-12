98ea8d624d719b63974ecd51d51b7a06d0c5b95b
ExtranoDev <jeomade19@gmail.com>
2022-11-12 17:31:25 +0000

    Filter states

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Connects to database and list items in database
+fiters result using search keyword"""
+
+
+import MySQLdb
+from sys import argv
+
+if __name__ == '__main__':
+    db = MySQLdb.connect(user=argv[1], passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    query = "SELECT * FROM states WHERE name='{}' ORDER BY id".format(argv[4])
+    cur.execute(query)
+    while (1):
+        row = cur.fetchone()
+        if not row:
+            break
+        print(row)
